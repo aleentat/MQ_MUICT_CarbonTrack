dd8d54c27f2f549e192cddc580a5be7789b19724
Aleenta <aleenta.tan@student.mahidol.ac.th>
2025-07-11 23:48:32 +1000

    Add sqlite db

---
+++ lib/models/waste_diary_entry.dart
@@ -0,0 +1,31 @@
+class WasteDiaryEntry {
+  final int? id;
+  final String name;
+  final String type;
+  final DateTime timestamp;
+
+  WasteDiaryEntry({
+    this.id,
+    required this.name,
+    required this.type,
+    required this.timestamp,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'type': type,
+      'timestamp': timestamp.toIso8601String(),
+    };
+  }
+
+  factory WasteDiaryEntry.fromMap(Map<String, dynamic> map) {
+    return WasteDiaryEntry(
+      id: map['id'],
+      name: map['name'],
+      type: map['type'],
+      timestamp: DateTime.parse(map['timestamp']),
+    );
+  }
+}
